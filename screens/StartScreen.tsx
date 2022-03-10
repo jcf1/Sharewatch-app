eba5d9cc55d0e8c928817954504210754517fe4c
jcf1 <john_free@aol.com>
2022-03-10 00:45:08 -0600

    Added UI to the StartScreen

--- screens/StartScreen.tsx
+++ screens/StartScreen.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, View, Text, TouchableHighlight } from 'react-native';
+import React, {FC, useState} from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableHighlight} from 'react-native';
 
 interface Props {
     beginWatch: () => void;
@@ -9,13 +9,101 @@ interface Props {
 }
 
 
-export const StartScreen: React.FC<Props> = ({beginWatch,setOffline,setCreate,setRoomCode}) => {
+export const StartScreen: FC<Props> = ({beginWatch,setOffline,setCreate,setRoomCode}) => {
+    const [code, setCode] = useState('');
+
+    function joinPress() {
+
+        setRoomCode(code);
+        beginWatch();
+    }
+
+    function createPress() {
+        setCreate(true);
+        beginWatch();
+    }
+
+    function offlinePress() {
+        setOffline(true);
+        beginWatch();
+    }
+
     return(
-        <>
-            <Text>START SCREEN</Text>
-            <TouchableHighlight onPress={beginWatch}>
-                <Text>START</Text>
-            </TouchableHighlight>
-        </>
+        <View style={styles.body}>
+            <View style={styles.heading}>
+                <Text style={styles.title}>ShareWatch</Text>
+            </View>
+
+            <View>
+                <View style={styles.joinView}>
+                    <TextInput style={styles.joinInput} placeholder="CODE" autoCapitalize="characters" autoCompleteType="off" maxLength={4}/>
+                    <TouchableHighlight style={styles.joinButton}>
+                        <Text style={styles.buttonText}>Join Room</Text>
+                    </TouchableHighlight>
+                </View>
+                    <TouchableHighlight style={styles.fullButton}>
+                        <Text style={styles.buttonText}>Create Room</Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight style={styles.fullButton}>
+                        <Text style={styles.buttonText}>Offline StopWatch</Text>
+                    </TouchableHighlight>
+            </View>
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    body: {
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#2B2B2B',
+    },
+    heading: {
+        marginBottom: 20,
+        borderBottomWidth: 2,
+        borderBottomColor: '#ffffff'
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 55,
+        fontWeight: 'bold',
+        color: '#ffffff',
+    },
+    joinView: {
+        alignItems: 'flex-start',
+        flexDirection: 'row',
+    },
+    joinInput: {
+        backgroundColor: '#ffffff',
+        textAlign: "center",
+        fontSize: 40,
+        minWidth: 145,
+        minHeight: 75,
+        marginRight:10,
+        borderWidth: 1,
+    },
+    joinButton: {
+        backgroundColor: '#3399ff',
+        justifyContent: 'center',
+        minWidth: 145,
+        minHeight: 75,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+    fullButton: {
+        backgroundColor: '#3399ff',
+        justifyContent: 'center',
+        minWidth: 300,
+        minHeight: 75,
+        marginTop: 25,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+    buttonText: {
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#ffffff',
+    },
+});
